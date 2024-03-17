862f210dd4c9b519b0f6f9f1fd91feba9ab7a41b
BarathT <barathth@lentra.ai>
2024-03-18 04:38:55 +0530

    initial pwa

--- src/Users.jsx
+++ src/Users.jsx
@@ -1,8 +1,30 @@
 import { useQuery } from "@tanstack/react-query";
 import { getVoted } from "./api";
+import { useState } from "react";
 
 function Users({ id }) {
-    const query = useQuery({ queryKey: ['users'], queryFn: () => getVoted(), refetchInterval: 20 * 500 });
+    const [voted, setVoted] = useState(false);
+    const query = useQuery({
+        queryKey: ['users'],
+        queryFn: () => getVoted(),
+        refetchInterval: 500 * 2,
+    });
+    if (!voted && query?.data?.data) {
+        if (id in query.data.data) {
+            setVoted(true);
+            if ('Notification' in window) {
+                Notification.requestPermission().then((permission) => {
+                    if (permission === 'granted') {
+                        new Notification('From voting system', {
+                            body: 'your vote has been registered',
+                            //icon: '/path/to/notification/icon.png',
+                        });
+                    }
+                });
+            }
+        }
+    }
+
     return (<>
         {query?.data?.data ?
             <>{(id in query?.data?.data) ? <div>your vote has been registered!</div> : <div>your vote is not registered</div>}</> : <div>some problem with site!</div>
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +10,25 @@ export default defineConfig({
       '*/api': 'http://localhost:3001'
     }
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "voting system",
+        short_name: "voting system",
+        start_url: "/",
+        display: "standalone",
+        theme_color: '#000000',
+        icons: [
+          { src: "./vite.png", sizes: "512x512", type: "image/png", purpose: "any maskable" },
+          { src: "./vite.svg", sizes: "192x192", type: "image/svg+xml", purpose: "any maskable" }
+        ],
+      },
+      prefer_related_applications: true,
+      injectRegister: 'auto',
+      registerType: 'autoUpdate',
+      workbox: {},
+      notification: true
+    })
+  ],
 })
